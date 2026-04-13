776e2bf689ef60fd8950f9885944927854852e25
prakashghorpade <prakashghorpade2001@gmail.com>
2026-04-13 22:46:39 +0530

    added the iam user resource

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
 resource "aws_iam_user" "users" {
-  name = "loadbalancer"
-  path = "/system/"
+  
+  for_each = {  for user in local.users : user.first_name => user }
+
+  name = lower("${substr(each.value.first_name,0,1)}${each.value.last_name}")
+  path = "/users"
 
   tags = {
-    tag-key = "tag-value"
+    DisplayName = "${each.value.first_name}${each.value.last_name}"
+    Department = each.value.department
+    Jobtitle = each.value.job_title
   }
+
 }
