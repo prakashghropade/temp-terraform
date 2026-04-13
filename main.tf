a1e35537464efcdf20b2aae70efe7b9eb2d20b67
prakashghorpade <prakashghorpade2001@gmail.com>
2026-04-13 23:50:01 +0530

    updated the password variable

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_iam_user" "users" {
   for_each = {  for user in local.users : user.first_name => user }
 
   name = lower("${substr(each.value.first_name,0,1)}${each.value.last_name}")
-  path = "/users"
+  path = "/users/"
 
   tags = {
     DisplayName = "${each.value.first_name}${each.value.last_name}"
